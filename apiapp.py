5cb9bae714fc3153192b58dac62832c963943b11
N00000027 N00000027 <n00000027@IF4000668028N1.local>
2023-03-07 22:45:03 +0700

    Initial comment

---
+++ apiapp.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+app = FastAPI()
+richest_people_list =  {
+	"Elon Musk": "280 Billion USD",
+	"Jeff Bezos": "250 Billion USD",
+	"Bill Gates": "190 Billion USD",
+	"Mark Zukerberg": "150 Billion USD",
+}
+@app.get("/richest-people")
+def richest_people():
+	return richest_people_list
